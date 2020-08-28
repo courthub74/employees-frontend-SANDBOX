fc5101160efcf1ad903cb3411a3d557458379764
Courtney Jacobs <cjacobs124@gmail.com>
2020-08-28 15:44:43 -0400

    quote output

--- employeefront/views.py
+++ employeefront/views.py
@@ -7,4 +7,8 @@ def home(request):
 	import json
 	employee_request = requests.get("http://nexcouremployees.courdevelops.com/employees/?format=json")
 	employee = json.loads(employee_request.content)
-	return render(request, 'home.html', {'employee': employee})
+	if request.method == 'POST':
+		quote = request.POST['quote']
+		return render(request, 'home.html', {'quote': quote})
+	else:
+		return render(request, 'home.html', {})
