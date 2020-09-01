51b84e402a328cb1bb070b579e222babce60d8d7
Courtney Jacobs <cjacobs124@gmail.com>
2020-09-01 19:14:18 -0400

    Global variable working

--- employeefront/urls.py
+++ employeefront/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.emp1, name="home"),
+    path('2/', views.emp2, name="emp2"),
 ]
--- employeefront/views.py
+++ employeefront/views.py
@@ -1,21 +1,44 @@
 from django.shortcuts import render
 from django.contrib import messages
 
+
 #HOME
-def home(request):
+def emp1(request):
 	import requests
 	import json
-	employee_request = requests.get("http://nexcouremployees.courdevelops.com/employees/?format=json")
-	employee = json.loads(employee_request.content)
-	if request.method == 'POST':
-		quote = request.POST['quote']
-		employee_direct = requests.get("http://nexcouremployees.courdevelops.com/employees/" + quote + "?format=json")
-		direct = json.loads(employee_direct.content)
 
-		employee1_direct = requests.get("http://nexcouremployees.courdevelops.com/employees/" + quote)
-		employee1 = json.loads(employee1_direct.content)
-		
-	
-		return render(request, 'home.html', {'quote': quote, 'direct': direct, 'employee1': employee1})
+	if request.method == 'POST':
+		quote = request.POST['quote'] # or None
+
+		if quote == "10001401":
+			employee1 = requests.get("http://nexcouremployees.courdevelops.com/employees/1/?format=json")
+			global direct1
+			direct1 = json.loads(employee1.content)
+
+			return render(request, 'home.html', {'quote': quote, 'direct1': direct1})
+
 	else:
-		return render(request, 'home.html', {})
+		nothingyet = "NexCour Employees List Goes Here..."
+		return render(request, 'home.html', {'nothingyet': nothingyet})
+
+
+#HOME
+def emp2(request):
+	import requests
+	import json
+
+	if request.method == 'POST':
+		quote = request.POST['quote'] # or None
+
+		if quote == "10001404":
+			employee2 = requests.get("http://nexcouremployees.courdevelops.com/employees/2/?format=json")
+			global direct2
+			direct2 = json.loads(employee2.content)
+
+			return render(request, 'home2.html', {'quote': quote, 'direct2': direct2})
+
+	else:
+		nothingyet = "NexCour Employees List Goes Here..."
+		return render(request, 'home2.html', {'nothingyet': nothingyet})
+
+
